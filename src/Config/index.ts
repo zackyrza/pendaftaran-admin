4c41781ea7bf969247a392f90011fd8ed7a13217
Rifqy Zacky Ariadhy <rifqyzackya@gmail.com>
2023-02-09 21:30:04 +0700

    change config domain

--- src/Config/index.ts
+++ src/Config/index.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:8000/api';
-export const IMAGE_URL = 'http://localhost:8000/';
+export const API_URL = 'http://pendaftaran-backend.mitraniagateknologi.com/api';
+export const IMAGE_URL = 'http://pendaftaran-backend.mitraniagateknologi.com/';
